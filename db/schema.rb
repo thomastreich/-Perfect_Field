24dc4ace10026cf637aef1cf99c668a0fdd8d908
Jeremy Dumas <mulot90@hotmail.com>
2020-02-17 15:34:22 +0100

    create moissonneuse model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_17_133329) do
+ActiveRecord::Schema.define(version: 2020_02_17_141112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "moissonneuses", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "brand"
+    t.string "model"
+    t.text "description"
+    t.integer "power"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_moissonneuses_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +40,5 @@ ActiveRecord::Schema.define(version: 2020_02_17_133329) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "moissonneuses", "users"
 end
