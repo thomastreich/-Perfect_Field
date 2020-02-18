5887befb77122fa9ccb6d780b32efd5693bb6807
astrid aubert <astridaubert@gmail.com>
2020-02-18 14:53:54 +0100

    pundit set up user

--- app/controllers/moissonneuses_controller.rb
+++ app/controllers/moissonneuses_controller.rb
@@ -1,10 +1,11 @@
 class MoissonneusesController < ApplicationController
   def index
-    @moissonneuses = Moissonneuse.all
+    @moissonneuses = policy_scope(Moissonneuse).order(created_at: :desc)
   end
 
     def show
       @moissonneuse = Moissonneuse.find(params[:id])
+      authorize @moissonneuse
     end
 
   def new
@@ -14,11 +15,31 @@ class MoissonneusesController < ApplicationController
   def create
     @moissonneuse = Moissonneuse.new(moissonneuse_params)
     @moissonneuse.user = current_user
+    authorize @moissonneuse
     if @moissonneuse.save!
       redirect_to moissonneuse_path(@moissonneuse)
     else
       render :new
     end
+  end
+
+  def edit
+   @moissonneuse = Moissonneuse.find(params[:id])
+  end
+
+  def update
+    @moissonneuse = Moissonneuse.find(params[:id])
+    @moissonneuse.user = current_user
+    @moissonneuse.update(moissonneuse_params)
+    redirect_to moissonneuse_path(@moissonneuse)
+  end
+
+  end
+
+  def destroy
+    @moissonneuse = Moissonneuse.find(params[:id])
+    @moissonneuse.destroy
+    redirect_to root_path
   end
 
   private
---
+++ app/policies/moissonneuse_policy.rb
@@ -0,0 +1,19 @@
+class MoissonneusePolicy < ApplicationPolicy
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
