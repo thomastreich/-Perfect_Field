f1c1dedfa883e4bc10dc9c2bb82b2e09df7da844
astrid aubert <astridaubert@gmail.com>
2020-02-18 18:25:54 +0100

    creation of a seed of 4 users and 12 moissonneuses

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,9 @@ User.destroy_all
 puts 'Creating users...'
 
 user_1 = User.create!(email: "astrid@gmail.com", password: "Astrid")
-user_2 = User.create!(email: "astridaubert@gmail.com", password: "AstridAubert")
-user_3 = User.create!(email: "thomas@gmail.com", password: "Thomas")
+user_2 = User.create!(email: "thomas@gmail.com", password: "Thomas")
+user_3 = User.create!(email: "jeremy@gmail.com", password: "Jeremy")
+user_4 = User.create!(email: "pierre@gmail.com", password: "Pierre")
 
 puts 'Finished users creation!'
 
@@ -82,5 +83,77 @@ user: user_3)
 file = File.open([File.join(File.dirname(__FILE__), '../app/assets/images/moissonneuse_6.jpeg')].first)
 moissonneuse_6.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 moissonneuse_6.save!
+
+moissonneuse_7 = Moissonneuse.new(
+name: 'Moissonneuse-batteuse 2014',
+description: 'Jolie machine. Bon rapport qualité prix. Top du moment. Jolie machine. Bon rapport qualité prix. Top du moment',
+address: %w(75000 77000 37000 21000 38000).sample,
+brand: ["Massey Ferguson", "John Deer", "Claas", "New Holland", "Case IH", "Fendt", "Dodge", "Laverda", "Deutz-Fahr"].sample,
+model: %w(V100 V210 SL200 SQ300 SL560).sample,
+power: %w(250, 350, 400, 500).sample,
+user: user_4)
+file = File.open([File.join(File.dirname(__FILE__), '../app/assets/images/moissonneuse_7.jpeg')].first)
+moissonneuse_7.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+moissonneuse_7.save!
+
+moissonneuse_8 = Moissonneuse.new(
+name: 'Moissonneuse-batteuse 2013',
+description: 'Jolie machine. Bon rapport qualité prix. Top du moment. Jolie machine. Bon rapport qualité prix. Top du moment',
+address: %w(75000 77000 37000 21000 38000).sample,
+brand: ["Massey Ferguson", "John Deer", "Claas", "New Holland", "Case IH", "Fendt", "Dodge", "Laverda", "Deutz-Fahr"].sample,
+model: %w(V100 V210 SL200 SQ300 SL560).sample,
+power: %w(250, 350, 400, 500).sample,
+user: user_4)
+file = File.open([File.join(File.dirname(__FILE__), '../app/assets/images/moissonneuse_8.jpeg')].first)
+moissonneuse_8.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+moissonneuse_8.save!
+
+moissonneuse_9 = Moissonneuse.new(
+name: 'Moissonneuse-batteuse 2012',
+description: 'Jolie machine. Bon rapport qualité prix. Top du moment. Jolie machine. Bon rapport qualité prix. Top du moment',
+address: %w(75000 77000 37000 21000 38000).sample,
+brand: ["Massey Ferguson", "John Deer", "Claas", "New Holland", "Case IH", "Fendt", "Dodge", "Laverda", "Deutz-Fahr"].sample,
+model: %w(V100 V210 SL200 SQ300 SL560).sample,
+power: %w(250, 350, 400, 500).sample,
+user: user_1)
+file = File.open([File.join(File.dirname(__FILE__), '../app/assets/images/moissonneuse_9.jpeg')].first)
+moissonneuse_9.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+moissonneuse_9.save!
+
+moissonneuse_10 = Moissonneuse.new(
+name: 'Moissonneuse-batteuse 2011',
+description: 'Jolie machine. Bon rapport qualité prix. Top du moment. Jolie machine. Bon rapport qualité prix. Top du moment',
+address: %w(75000 77000 37000 21000 38000).sample,
+brand: ["Massey Ferguson", "John Deer", "Claas", "New Holland", "Case IH", "Fendt", "Dodge", "Laverda", "Deutz-Fahr"].sample,
+model: %w(V100 V210 SL200 SQ300 SL560).sample,
+power: %w(250, 350, 400, 500).sample,
+user: user_1)
+file = File.open([File.join(File.dirname(__FILE__), '../app/assets/images/moissonneuse_10.jpeg')].first)
+moissonneuse_10.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+moissonneuse_10.save!
+
+moissonneuse_11 = Moissonneuse.new(
+name: 'Moissonneuse-batteuse 2010',
+description: 'Jolie machine. Bon rapport qualité prix. Top du moment. Jolie machine. Bon rapport qualité prix. Top du moment',
+address: %w(75000 77000 37000 21000 38000).sample,
+brand: ["Massey Ferguson", "John Deer", "Claas", "New Holland", "Case IH", "Fendt", "Dodge", "Laverda", "Deutz-Fahr"].sample,
+model: %w(V100 V210 SL200 SQ300 SL560).sample,
+power: %w(250, 350, 400, 500).sample,
+user: user_2)
+file = File.open([File.join(File.dirname(__FILE__), '../app/assets/images/moissonneuse_11.jpeg')].first)
+moissonneuse_11.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+moissonneuse_11.save!
+
+moissonneuse_12 = Moissonneuse.new(
+name: 'Moissonneuse-batteuse 2009',
+description: 'Jolie machine. Bon rapport qualité prix. Top du moment. Jolie machine. Bon rapport qualité prix. Top du moment',
+address: %w(75000 77000 37000 21000 38000).sample,
+brand: ["Massey Ferguson", "John Deer", "Claas", "New Holland", "Case IH", "Fendt", "Dodge", "Laverda", "Deutz-Fahr"].sample,
+model: %w(V100 V210 SL200 SQ300 SL560).sample,
+power: %w(250, 350, 400, 500).sample,
+user: user_2)
+file = File.open([File.join(File.dirname(__FILE__), '../app/assets/images/moissonneuse_12.jpeg')].first)
+moissonneuse_12.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+moissonneuse_12.save!
 
 puts 'Finished moissonneuses creation!'
