8e4ca0cffd0782e55e53f69af945c0219e7b8799
Thomas Streich <tstreich@hotmail.fr>
2020-02-18 15:37:47 +0100

    commit

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,11 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :moissonneuse
-  validates :starting_date, :ending_date, presence: true if dates?
+  validates :starting_date, :ending_date, presence: true
   validates :moissonneuse, uniqueness: { scope: :user }
 
-  def dates?
-    starting_date < ending_date
-  end
+  # def dates?
+  #   starting_date < ending_date
+  # end
 
 end
