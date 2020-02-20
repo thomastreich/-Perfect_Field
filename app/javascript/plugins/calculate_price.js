b10cce54791c15bc6cd64ba4bbde4ff54df7aa3b
Thomas Streich <tstreich@hotmail.fr>
2020-02-20 12:33:01 +0100

    javascript-not-resolute

---
+++ app/javascript/plugins/calculate_price.js
@@ -0,0 +1,16 @@
+const calculatePrice = () => {
+  // Recuperer tous les inputs
+  const year = document.getElementById("booking_starting_date_1i").value;
+  const month = document.getElementById("booking_starting_date_2i").value;
+  const day = document.getElementById("booking_starting_date_3i").value;
+  // On recupere le prix par jour
+
+
+  // On ecoute le change des inputs de dates
+    // Parse les dates de debut et de fin
+    // On calcule le nombre de jours
+    // On calcule le prix total
+    // On l'insere dans le champ Price
+}
+
+export default calculatePrice;
