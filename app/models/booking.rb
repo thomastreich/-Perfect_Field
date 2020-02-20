cd83e283b49760b39cf4fdd03cde4f8b3bcc7cd9
Thomas Streich <tstreich@hotmail.fr>
2020-02-20 17:01:41 +0100

    booking-index

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,12 +2,28 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :moissonneuse
   validates :starting_date, :ending_date, presence: true
-  validates :moissonneuse, uniqueness: { scope: :user }
+  validate :moissonneuse_date_uniqueness
+  # validates :moissonneuse, uniqueness: { scope: :user }
 
   attr_accessor :price
+
+  scope :past, -> { where("? > ending_date", Date.today)}
+  scope :current, -> { where("? BETWEEN starting_date AND ending_date", Date.today)}
+  scope :future, -> { where("? < ending_date", Date.today)}
+
+  def length_in_days
+    (ending_date - starting_date + 1).to_i
+  end
+
+  def price
+    moissonneuse.price * length_in_days if moissonneuse
+  end
+
+  def moissonneuse_date_uniqueness
+
+  end
+end
 
   # def dates?
   #   starting_date < ending_date
   # end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :moissonneuses do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:create]
   end
+
+  resources :bookings, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
