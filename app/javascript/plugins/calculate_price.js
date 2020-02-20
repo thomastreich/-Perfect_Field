2ab9abbf0958fae8b573e5c6530afe97fd29ed28
Thomas Streich <tstreich@hotmail.fr>
2020-02-20 17:17:49 +0100

    js-update

--- app/javascript/plugins/calculate_price.js
+++ app/javascript/plugins/calculate_price.js
@@ -4,13 +4,20 @@ const calculatePrice = () => {
     const start_year = document.getElementById("booking_starting_date_1i").value;
     const start_month = document.getElementById("booking_starting_date_2i").value;
     const start_day = document.getElementById("booking_starting_date_3i").value;
+//
+    // console.log(start_year, start_month, start_day)
+
     const start_date = start_year + " " + start_month + " " + start_day;
     const parse_start_date = Date.parse(start_date);
 
     const end_year = document.getElementById("booking_ending_date_1i").value;
     const end_month = document.getElementById("booking_ending_date_2i").value;
     const end_day = document.getElementById("booking_ending_date_3i").value;
+
+    // console.log(end_year, end_month, end_day)
+
     const end_date = end_year + " " + end_month + " " + end_day;
+    console.log(end_date)
     const parse_end_date = Date.parse(end_date);
 
     const price_moisso = document.querySelector('.card-trip-pricing').dataset.price;
